86a192b9d8923eaf6452bff1e3487dba287168dd
Veronica Morales <vmj1918@gmail.com>
2021-07-14 14:17:12 -0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: 'antonio', email: "antonio@gmail.com", password: "2435")
+Product.create(name: "Refresco", price: 16.00, description: "Cocacola")
+Product.create(name: "Sabritas", price: 12.00, description: "Rufles")
